b9f6a3dea5dd940f74a10e143b714cd04ed519dd
Dricks <hendwrickgonzales@gmail.com>
2022-01-19 18:43:11 +0800

    Updated init file

--- php/init.php
+++ php/init.php
@@ -2,6 +2,6 @@
   require_once 'functions.php';
 
   spl_autoload_register(function($class){
-    require_once $_SERVER['DOCUMENT_ROOT'].'/Inventory_POS_Pielyn/class/'.$class.'.php';
+    require_once $_SERVER['DOCUMENT_ROOT'].'/Inventory-POS-Pielyn/class/'.$class.'.php';
   });
 ?>
